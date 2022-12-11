a397de7fa84331fe934ea663d2e0e9cd0b443c9c
Rayala Akhil <rayalaakhil1999@gmail.com>
2022-12-11 16:45:11 +0530

    Added a class and methods (generate())

--- Password Generator.py
+++ Password Generator.py
@@ -1,23 +1,14 @@
 import random
 
-print("Welcome to password generator!")
-
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()+<>?,./1234567890'
-
-number = int(input('Enter the no. of passwords you want to generate: '))
-
-if number > 0:
-    length = int(input('Enter the length of your password: '))
-
-if number == 1:
-    print('\nHere is your password:')
-elif number <= 0:
-    print('Enter a valid number!!!!')
-else:
-    print('\nHere are your passwords:')
-
-for pwd in range(number):
+class Generate_pass:
+    pass_Length = 0
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()+<>?,./1234567890'
     password = ''
-    for char in range(length):
-        password += random.choice(chars)
-    print(password)
+
+    def __init__(self, pass_Length) -> None:
+        self.pass_Length = pass_Length
+
+    def generate(self) -> str:
+        for char in range(self.pass_Length):
+            self.password += char
+        return self.password
