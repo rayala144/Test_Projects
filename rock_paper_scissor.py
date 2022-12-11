1c7218505e0f25fd750fd133ca404faddddf1a5c
Rayala Akhil <rayalaakhil1999@gmail.com>
2022-12-11 18:23:29 +0530

    Cleaning code

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,7 +1,8 @@
 import random
 
+
 def game():
-    user = input("What's your choice? (r) for rock, (p) for papar, (s) for scissors: ")
+    user = input("What's your choice? (r) for rock, (p) for paper, (s) for scissors: ")
     computer = random.choice(['r', 'p', 's'])
     
     if user == computer:
@@ -16,13 +17,12 @@ def game():
     return 
 
 
-
-
 def has_won(player, opponent):
     # return 'true' if player wins
     # r > s, s > p, p > r
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-        or (player == 'p' and opponent == 'r'):
+            or (player == 'p' and opponent == 'r'):
         return True
 
-game()   
+
+game()
