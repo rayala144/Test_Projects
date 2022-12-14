f8f19a0b53b13df851626f7629bf85bed70b6853
Rayala Akhil <64125982+rayala144@users.noreply.github.com>
2022-12-14 15:36:45 +0530

    Cleaned code

--- web scraping.py
+++ web scraping.py
@@ -5,4 +5,6 @@ url = "https://github.com/" + github_user
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
 profile_image = soup.find('img', {'alt' : 'Avatar'})['src']
-print (profile_image)
+
+if __name__ == '__main__':
+    print(profile_image)
