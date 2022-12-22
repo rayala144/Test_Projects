9053922b8a8aa97157de6de38b16847c8fdc2224
rayala144 <rayalaakhil1999@gmail.com>
2022-12-22 12:55:27 +0530

    Adding time module to the sorting file

--- brutalSorting.py
+++ brutalSorting.py
@@ -1,17 +1,22 @@
 import random
+import time
 
 
 def sort_one_billion_random_numbers():
+
+    # start time
+    start = time.time()
+
     # Generate a list of one billion random numbers
-    random_numbers = [random.randint(0, 1000000000) for i in range(1000000000)]
+    random_numbers = [random.randint(0, 1000000) for i in range(100000000)]
 
     # Sort the list of numbers
     sorted_numbers = sorted(random_numbers)
 
-    if (sorted_numbers != None):
-        return True
-    else:
-        return False
-        
+    # end time
+    end = time.time()
+    print(f'Time taken: {abs(start - end)}')
+
+
 if __name__ == "__main__":
     sort_one_billion_random_numbers()
