e9528bd99fc46d4f488923505b878b93b9cec352
rakhil <rakhil@cisco.com>
2023-01-08 19:11:29 +0530

    empty commit

--- C_programs/main.c
+++ C_programs/main.c
@@ -4,11 +4,11 @@ int main()
 {
     // Write C code here
     int i = 0;
-    while (i < 1000000000)
+    while (i < 10000000)
     {
         i++;
+
     }
     printf("%d", i);
-
     return 0;
 }
---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 1000) {
+			i++;
+		}
+		System.out.println(i);
+	}
+
+}
