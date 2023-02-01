db7aeefd4e06102aeada14a3a52c91d79989838c
rakhil <rakhil@cisco.com>
2023-02-01 18:13:33 +0530

    Changes

--- brutalSorting.py
+++ brutalSorting.py
@@ -1,5 +1,6 @@
 import random
 import time
+import numpy as np
 
 
 def sort_one_billion_random_numbers():
@@ -8,10 +9,10 @@ def sort_one_billion_random_numbers():
     start = time.time()
 
     # Generate a list of one billion random numbers
-    random_numbers = [random.randint(0, 1000000) for i in range(100000000)]
+    random_numbers = np.array([random.randint(0, 1000000) for i in range(10000000)])
 
     # Sort the list of numbers
-    sorted_numbers = sorted(random_numbers)
+    sorted(random_numbers)
 
     # end time
     end = time.time()
@@ -20,3 +21,4 @@ def sort_one_billion_random_numbers():
 
 if __name__ == "__main__":
     sort_one_billion_random_numbers()
+    
