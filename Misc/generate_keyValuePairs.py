7f0096459abfd005e42da9ea48cbac977b0b0147
rakhil <rakhil@cisco.com>
2023-06-07 18:18:55 +0530

    Updated generate_keyValuePairs.py

--- Misc/generate_keyValuePairs.py
+++ Misc/generate_keyValuePairs.py
@@ -5,23 +5,21 @@ def return_string_in_double_quotes(string: str):  # using single quotes
     return '"' + string + '"'  # enclosing the string within double quotes
 
 
-def extract_strings(input_string):
-    pattern = r"label:\s+'([^']*)'"
+def extract_strings(input_string, pattern):
     matches = re.findall(pattern, input_string)
     return matches
 
-def extract_strings_from_file(file_path):
+def extract_strings_from_file(file_path, regex_pattern):
     with open(file_path, 'r') as file:
         input_string = file.read()
-    output_string = extract_strings(input_string)
+    output_string = extract_strings(input_string, regex_pattern)
     return output_string
 
 
 if __name__ == "__main__":
-    # Example usage
-    file_path = sys.argv[1]
-    # file_path = r"C:\vManage\lat_script_pr_2\nms\src\main\client\src-ng2\app\configuration\templates\groups\mini-workflows\feature-profile\profile-parcel\logging-parcel\logging-parcel.constant.ts"
-    output = extract_strings_from_file(file_path)
+    
+    file_path = sys.argv[1]    
+    output = extract_strings_from_file(file_path, r"label:\s+'([^']*)'")
     
     for i in output:
         if i != "":
