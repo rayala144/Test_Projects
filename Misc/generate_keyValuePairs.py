aa9c68b9df02581ef2ab1c5480731632c19096cf
rakhil <rakhil@cisco.com>
2023-06-07 12:17:48 +0530

    Added a file

---
+++ Misc/generate_keyValuePairs.py
@@ -0,0 +1,27 @@
+import re
+
+def return_string_in_double_quotes(string: str):  # using single quotes
+    return '"' + string + '"'  # enclosing the string within double quotes
+
+
+def extract_strings(input_string):
+    pattern = r"label:\s+'([^']*)'"
+    matches = re.findall(pattern, input_string)
+    return matches
+
+def extract_strings_from_file(file_path):
+    with open(file_path, 'r') as file:
+        input_string = file.read()
+    output_string = extract_strings(input_string)
+    return output_string
+
+
+if __name__ == "__main__":
+    # Example usage
+    file_path = r"C:\vManage\lat_script_pr_2\nms\src\main\client\src-ng2\app\configuration\templates\groups\mini-workflows\feature-profile\profile-parcel\logging-parcel\logging-parcel.constant.ts"
+    output = extract_strings_from_file(file_path)
+    
+    for i in output:
+        print(f"    {return_string_in_double_quotes(i)}: {return_string_in_double_quotes(i)},")
+
+
